d491ad143b061f4175084e4c3f79a7f50243e4fd
Marek Counts <mcounts@suse.com>
2020-02-28 14:08:40 +0000

    started stream/parsing/thing manager

--- chat/src/clients/parsers/parser.rs
+++ chat/src/clients/parsers/parser.rs
@@ -1,4 +1,18 @@
+
+
 pub struct Parser {
+    sender: tokio::sync::mpsc::UnboundedSender<Message>,
+    ws_done_join_handle: tokio::task::JoinHandle<()>,
+}
 
+pub enum EventType {
+    All,
+    PrivateMsg,
 
+}
+
+impl Parser {
+    pub fn listen(type: EventType) -> tokio::sync::mpsc::UnboundedResiver<Message> {
+
+    }
 }
